c43949fd134e99a1a83e941dde4036170b656911
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2024-03-17 10:32:46 +0530

    Added sample project 2 backend

---
+++ content-calendar/content-calendar/content-calendar/src/main/java/com/gayanukaa/contentcalendar/config/MyWebConfig.java
@@ -0,0 +1,16 @@
+package com.gayanukaa.contentcalendar.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration // When to define a bean it is inside a configuration class
+public class MyWebConfig {
+
+    @Bean //Spring will manage this bean. It will be available for autowiring
+    //RestTemplate - client to call out to public API within organisation
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder().build(); // return new instance of RestTemplate
+    }
+}
---
+++ content-calendar/content-calendar/content-calendar/src/test/java/com/gayanukaa/contentcalendar/ContentCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gayanukaa.contentcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
