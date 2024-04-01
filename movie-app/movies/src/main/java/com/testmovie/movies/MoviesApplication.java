b4df025f32df3d47c7494f20e597efce2b0c8937
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2024-04-01 11:06:02 +0530

    Initial implementations

--- movie-app/movies/src/main/java/com/testmovie/movies/MoviesApplication.java
+++ movie-app/movies/src/main/java/com/testmovie/movies/MoviesApplication.java
@@ -3,11 +3,16 @@ package com.testmovie.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//annotations tell the compiler what this class does
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MoviesApplication.class, args);
+		//this is the main method that runs the application
+		//we pass the class name and the args to the SpringApplication.run method
+
 	}
 
 }
