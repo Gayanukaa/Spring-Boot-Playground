5721b4719c42517b4461e70b1b3fe01008362737
Gayanuka Amarasuriya <gayanukaamarasuriya@gmail.com>
2024-04-01 11:31:38 +0530

    Configured database

--- movie-app/movies/src/main/java/com/testmovie/movies/MoviesApplication.java
+++ movie-app/movies/src/main/java/com/testmovie/movies/MoviesApplication.java
@@ -2,9 +2,12 @@ package com.testmovie.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 //annotations tell the compiler what this class does
 @SpringBootApplication
+@RestController //lets the framework know this is a REST api controller and not a class
 public class MoviesApplication {
 
 	public static void main(String[] args) {
@@ -14,5 +17,14 @@ public class MoviesApplication {
 		//we pass the class name and the args to the SpringApplication.run method
 
 	}
+
+	//this is an endpoint that returns a string
+	//endpoint is a URL that the API can be accessed from
+	//this is a sample api
+	//normally we divide application into several layers - service, data access, api
+	/*@GetMapping("/root")
+	public String apiRoot(){
+		return "Welcome to the Movies API";
+	}*/
 
 }
